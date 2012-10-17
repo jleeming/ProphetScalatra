086556dba415e164910469c275fd719ae6ada3e2
Samoylenko A <s_a_g@mail.com>
2012-10-18 00:01:09 +0400

    first commit

---
+++ build.sbt
@@ -0,0 +1,27 @@
+organization := "$organization$"
+
+name := "$name$"
+
+version := "$version$"
+
+scalaVersion := "2.9.1"
+
+seq(webSettings :_*)
+
+port in container.Configuration := 8080
+
+scalacOptions ++= Seq("-unchecked", "-deprecation")
+
+libraryDependencies ++= Seq(
+  "org.scalatra" %% "scalatra" % "2.0.4",
+  "org.scalatra" %% "scalatra-scalate" % "2.0.4",
+  "net.liftweb" %% "lift-json" % "2.5-M1",
+  "net.liftweb" %% "lift-json-ext" % "2.5-M1",
+  "org.eclipse.jetty" % "jetty-webapp" % "7.4.5.v20110725" % "container",
+  "javax.servlet" % "servlet-api" % "2.5" % "provided",
+  "org.slf4j" % "slf4j-simple" % "1.6.1",
+  "org.scalatra" %% "scalatra-scalatest" % "2.0.4" % "test"
+)
+
+resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+
---
+++ src/main/scala/prophet/scalatra/RestApiServlet.scala
@@ -0,0 +1,50 @@
+package spike.scalatra
+
+import org.scalatra._
+import scalate.ScalateSupport
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
+
+case class User(name: String, age: Int)
+
+class RestApiServlet extends ScalatraServlet with ScalateSupport with JsonHelpers {
+  before("/*") {
+    contentType = "application/json;charset=UTF-8"
+  }
+
+  get("/currency/:currencyType") {
+    params("currencyType") match {
+      case currencyType => Json(User("test " + currencyType, 30))
+    }
+  }
+  
+  get("/api/json/users/:id") {
+    params("id") match {
+      case "1" => Json(User("John", 30))
+    }
+  }
+
+  post("/api/user") {
+    val user = parse(request.body).extract[User]
+    1
+  }
+
+  get("/error") {
+    throw new RuntimeException("oh noez")
+  }
+
+  error {
+    case e: Exception =>
+      // TODO: Use log instead of console
+      println("Unexpected error during http api call.", e)
+      status(500)
+      contentType = "application/json;charset=UTF-8"
+      compact(render(("message" -> "Internal error.")))
+  }
+
+  notFound {
+    contentType = "application/json;charset=UTF-8"
+    status(404)
+    compact(render(("message" -> "Not found.")))
+  }
+}
